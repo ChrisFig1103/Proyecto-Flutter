a3cfcfcab23e74e124102c0fa89f63da1e35d770
ChrisFig1103 <christianfigueroamontano@outlook.com>
2023-05-02 07:38:24 -0600

    add_view structure

---
+++ lib/add_view.dart
@@ -0,0 +1,47 @@
+import 'package:crypto_wallet/net/flutterfire.dart';
+import 'package:flutter/material.dart';
+
+class AddView extends StatefulWidget {
+  AddView({Key key}) : super(key: key);
+
+  @override
+  _AddViewState createState() => _AddViewState();
+}
+
+class _AddViewState extends State<AddView> {
+  List<String> stocks = [
+    "stock_1",
+    "stock_2",
+    "stock_3",
+  ];
+
+  String dropdownValue = "stock_1";
+  TextEditingController _amountController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          DropdownButton(
+            value: dropdownValue,
+            onChanged: (String value) {
+              setState(() {
+                dropdownValue = value;
+              });
+            },
+            items: coins.map<DropdownMenuItem<String>>((String value) {
+              return DropdownMenuItem<String>(
+                value: value,
+                child: Text(value),
+              );
+            }).toList(),
+          ),
+          
+           
+        ],
+      ),
+    );
+  }
+}
